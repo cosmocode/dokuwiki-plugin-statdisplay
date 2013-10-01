3d75226b2d7a80c49e43382471e42c9339c56a8b
Hideaki SAWADA <chuno@live.jp>
2013-10-01 16:15:04 +0200

    translation update

---
+++ lang/ja/lang.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['status_100']            = '継続';
+$lang['status_101']            = 'プロトコル切替え';
+$lang['status_200']            = 'OK';
+$lang['status_201']            = '作成';
+$lang['status_202']            = '受理';
+$lang['status_203']            = '信頼できない情報';
+$lang['status_204']            = '内容なし';
+$lang['status_205']            = '内容のリセット';
+$lang['status_206']            = '部分的内容';
+$lang['status_300']            = '複数の選択';
+$lang['status_301']            = '恒久的に移動した';
+$lang['status_302']            = '発見した';
+$lang['status_303']            = '他を参照せよ';
+$lang['status_304']            = '未更新';
+$lang['status_305']            = 'プロキシを使用せよ';
+$lang['status_306']            = '将来のために予約されている';
+$lang['status_307']            = '一時的リダイレクト';
+$lang['status_400']            = 'リクエストが不正である';
+$lang['status_401']            = '認証が必要である';
+$lang['status_402']            = '支払いが必要である';
