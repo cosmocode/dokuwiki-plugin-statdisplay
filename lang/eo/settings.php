ac76974c79d15399fc854914d48134434ff28842
Robert Bogenschneider <bogi@uea.org>
2014-03-01 08:40:05 +0100

    translation update

---
+++ lang/eo/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Robert Bogenschneider <bogi@uea.org>
+ */
+$lang['accesslog']             = 'Loko de la log-dosiero. Relativaj padoj relativas al <code>data/meta</code>';
+$lang['lines']                 = 'Nombro de linioj por analizi. Malpli grandaj nombroj donas pli rapide aliron al aktualaj datumoj kaj limigas memoruzadon, sed altigas la nombron de fonaj taskoj.';
