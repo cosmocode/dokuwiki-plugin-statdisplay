623ccac9ffe5c8a1f05b9f7ef46e64eabd6c3aa8
Robert Bogenschneider <robog@gmx.de>
2014-05-18 17:00:05 +0200

    translation update

---
+++ lang/eo/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Robert Bogenschneider <robog@gmx.de>
+ */
+$lang['accesslog']             = 'Loko de logdosiero. Relativaj padoj estas relativaj al <code>data/meta</code>';
+$lang['lines']                 = 'Kvanto da linioj por analizi. Malpli da linioj donas pli rapidan aliron al aktualaj datumoj kaj limigas memorforuzon, sed altigas la kvanton da fonaj taskoj.';
