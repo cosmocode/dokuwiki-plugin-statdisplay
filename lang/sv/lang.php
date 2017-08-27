eda88a587a67b318096df1fa17bf5012041f5163
Tor Härnqvist <tor@harnqvist.se>
2017-08-27 11:30:07 +0200

    translation update

---
+++ lang/sv/lang.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tor Härnqvist <tor@harnqvist.se>
+ */
+$lang['t_statisticMonth']      = 'Månatlig statistik för %s';
+$lang['t_topPages']            = 'Toppsidor i %s';
+$lang['t_topEntry']            = 'Ingångsidor i topp i %s';
+$lang['t_topReferrer']         = 'Referenter i topp i %s';
+$lang['t_summary']             = 'Åtkomstsammanfattning per månad';
+$lang['t_month']               = 'Månatlig åtkomststatistik för %s';
+$lang['t_day']                 = 'Daglig åtkomststatistik för %s';
+$lang['t_hour']                = 'Timbaserad åtkomststatistik för %s';
+$lang['t_trafficday']          = 'Daglig besöksstatistik för %s';
+$lang['t_traffichour']         = 'Timbaserad besöksstatistik för %s';
+$lang['t_usertraffic']         = 'Sjudagars användartrafik';
+$lang['month']                 = 'Månad';
+$lang['day']                   = 'Veckodag';
+$lang['hour']                  = 'Timme';
+$lang['dailyavg']              = 'Daglig medel';
+$lang['totals']                = 'Månatlig totalsumma';
+$lang['all']                   = 'Alla';
+$lang['media']                 = 'Media';
+$lang['pages']                 = 'Sidor';
+$lang['visitors']              = 'Besökare';
+$lang['bytes']                 = 'Bytes';
+$lang['traffic']               = 'Frafik';
+$lang['avg']                   = 'medel';
+$lang['max']                   = 'max';
+$lang['files']                 = 'Filer';
+$lang['name']                  = 'Namn';
+$lang['others']                = '%d andra';
+$lang['totalFiles']            = 'Totalt antal filer';
+$lang['totalPages']            = 'Totalt antal sidor';
+$lang['totalVisitors']         = 'Totalt antal besökare';
+$lang['totalBytes']            = 'Totalt antal bytes';
+$lang['filesDay']              = 'Filer per dag';
+$lang['pagesDay']              = 'Sidor per dag';
+$lang['bytesDay']              = 'Bytes per dag';
+$lang['summaryMonth']          = 'Summering per månad';
+$lang['status_100']            = 'Fortsätt';
+$lang['status_101']            = 'Byter protokoll';
+$lang['status_200']            = 'OK';
+$lang['status_201']            = 'Skapad';
+$lang['status_202']            = 'Accepterad';
+$lang['status_204']            = 'Inget innehåll';
+$lang['status_205']            = 'Återställ innehåll';
+$lang['status_206']            = 'Delvis innehåll';
+$lang['status_300']            = 'Flera valmöjligheter';
+$lang['status_301']            = 'Permanent omdirigering';
+$lang['status_302']            = 'Omdirigera';
+$lang['status_303']            = 'Se andra';
+$lang['status_304']            = 'Inte modifierad';
+$lang['status_305']            = 'Använd proxy';
+$lang['status_306']            = 'Byt proxy';
+$lang['status_307']            = 'Temporär omdirigering';
+$lang['status_400']            = 'Felaktig begäran';
+$lang['status_402']            = 'Betalning krävs';
+$lang['status_403']            = 'Förbjudet';
+$lang['status_404']            = 'Ej funnet';
+$lang['status_405']            = 'Metoden ej tillåten';
+$lang['status_406']            = 'Icke-acceptabel';
+$lang['status_409']            = 'Konflikt';
+$lang['status_410']            = 'Borta';
+$lang['status_411']            = 'Längd krävs';
+$lang['status_414']            = 'Begärt URI för långt';
+$lang['status_449']            = 'Försök igen med';
+$lang['status_500']            = 'Internt serverfel';
+$lang['status_501']            = 'Ej implementerat';
+$lang['status_503']            = 'Tjänst ej tillgänglig';
+$lang['status_505']            = 'HTTP-version stöds ej';
